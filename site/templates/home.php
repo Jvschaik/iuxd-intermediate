b1dd1fb72ca0e6e8a04ef139452879f5cfca1f3b
Jvschaik <jsonschaik@gmail.com>
2019-11-24 18:54:09 +0100

    setup one page

---
+++ site/templates/home.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <?= css(['assets/css/style.css', '@auto']) ?>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title><?= $page->title() ?></title>
+  </head>
+
+  <body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#"><?= $page->title() ?></a>
+      <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Microinteractions</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Rules</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tools</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Impression</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Conclusion</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="row">
+      <div class="col">
+        <div class="header d-flex justify-content-center align-items-center">
+          <h1>Microinteractions</h1>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <div class="section-2 d-flex justify-content-center align-items-center">
+          <h1>"Building meaningful connections with users through design"</h1>
+        </div>
+      </div>
+    </div>
+
+    <div class="row section-3 no-gutters">
+      <div class="col-md-6 no-gutters">
+        <div class="leftside d-flex justify-content-center align-items-center">
+  
+        </div>
+      </div>
+      <div class="col-md-6 no-gutters">
+        <div class="rightside d-flex justify-content-center align-items-center">
+          <h2>Wat zijn micro microinterac?</h2>
+          <p>
+            Micro interacties zijn zoals de naam al zegt, hele kleine interacties. 
+            De micro interacties voeren 1 specifieke taak uit waarbij met gebruik van een animatie aan 
+            de gebruiker doorgegeven wordt wat er precies gebeurt tijdens de uitgevoerde taak. 
+            Het zijn kleine details in je digitale product die er voor zorgen dat de gebruiker een 
+            betere ervaring ervaart tijdens het gebruik van je product. 
+            Een voorbeeld van een micro interactie kan de airplane modus knop op je telefoon zijn. 
+            Als je op deze knop klikt verschuift het schuifje op de knop naar de andere kant en kleurt 
+            de achtergrond van de knop van grijs naar een andere kleur. 
+            Er is dus 1 taak wat er uitgevoerd wordt tijdens deze interactie en middels een animatie van zowel het 
+            schuifje als de kleur wordt aangeven wat er gebeurt tijdens de interactie.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row section-4 no-gutters">
+      <div class="col-md-6 no-gutters">
+        <div class="leftside d-flex justify-content-center align-items-center">
+          leftside
+        </div>
+      </div>
+      <div class="col-md-6 no-gutters">
+        <div class="rightside d-flex justify-content-center align-items-center">
+          click right side
+        </div>
+      </div>
+    </div>
+
+    <div class="row  section-5 no-gutters">
+      <div class="col-md-6 no-gutters">
+        <div class="leftside d-flex justify-content-center align-items-center">
+          leftside
+        </div>
+      </div>
+      <div class="col-md-6 no-gutters">
+        <div class="rightside d-flex justify-content-center align-items-center">
+          click right side
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <div class="section-2 d-flex justify-content-center align-items-center">
+          <h1>Conclusie</h1>
+        </div>
+      </div>
+    </div>
+
+    <footer class="page-footer font-small">
+      <div class="footer-copyright text-center py-3">© 2019 Copyright:
+        <a href="#"> Dennis Smits & Jason van Schaik</a>
+      </div>
+    </footer>
+  
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+  </body>
+
+</html>
